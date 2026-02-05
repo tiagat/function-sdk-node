46eaca13d68f6a50d9dd754f3adbc64f3b399407
Ruslan Sakal <ruslan.sakal@gmail.com>
2026-02-05 12:54:02 +0100

    control commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        '@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+    },
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
+    },
+    env: {
+        node: true,
+        es6: true,
+    },
+};
