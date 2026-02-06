e7b0d1f648cfb70d9990ae36c9ad2a075b4862b9
Ruslan Sakal <ruslan.sakal@gmail.com>
2026-02-06 20:23:16 +0100

    control commit

--- src/function-runtime-handler.ts
+++ src/function-runtime-handler.ts
@@ -17,3 +17,34 @@ function runEntrypoints(context: unknown): void {
 
 export { Handler };
 export { runEntrypoints };
+
+
+// const paramIndexByName = new Map<string, number>();
+
+// function FromContext(name: string): ParameterDecorator {
+//   return (target, propertyKey, parameterIndex) => {
+//     const key = `${String(propertyKey)}:${name}`;
+//     paramIndexByName.set(key, parameterIndex);
+//   };
+// }
+
+// function Handler(): MethodDecorator {
+//   return (_target, propertyKey, descriptor) => {
+//     const original = descriptor.value as (...args: any[]) => any;
+//     descriptor.value = function (...args: any[]) {
+//       const key = `${String(propertyKey)}:userId`;
+//       const index = paramIndexByName.get(key);
+//       if (index !== undefined) {
+//         args[index] = "injected-user-id";
+//       }
+//       return original.apply(this, args);
+//     };
+//   };
+// }
+
+// class Example {
+//   @Handler()
+//   run(@FromContext("userId") userId: string) {
+//     console.log(userId);
+//   }
+// }
