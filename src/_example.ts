25de39c41b7fa29fc76726bf651c8673d3733f6b
Ruslan Sakal <ruslan.sakal@gmail.com>
2026-02-08 13:28:43 +0100

    control commit

--- src/_example.ts
+++ src/_example.ts
@@ -1,17 +1,29 @@
-import { FunctionRuntime, Handler, Logger, Req, Res, RunFunctionRequest, RunFunctionResponse } from './index';
+import { FunctionRuntime, Handler, Logger, Req, Res, RunFunctionRequest, RunFunctionResponse, Resource, Ready } from './index';
 
 class MyFunction extends FunctionRuntime {
   logger = new Logger();
 
   @Handler()
-  main(): void {
-    this.logger.info('Function Handler (main)');
+  main(@Res() res: RunFunctionResponse): void {
+    const bucket: Resource = {
+      resource: {
+        apiVersion: 's3.aws.m.upbound.io/v1beta1',
+        kind: 'Bucket',
+      },
+      connectionDetails: {},
+      ready: Ready.READY_TRUE
+    };
+
+    if (res.desired?.resources) {
+      res.desired.resources['bucker'] = bucket;
+    } else {
+      res.desired = { resources: { bucker: bucket } };
+    }
   }
 
   @Handler()
   async mainAsync(@Req() req: RunFunctionRequest, @Res() res: RunFunctionResponse): Promise<void> {
-    this.logger.info({ req, res }, 'Request and Response');
-    this.logger.info('Function Handler (mainAsync)');
+    this.logger.info({ req, res }, 'Function Handler (mainAsync)');
   }
 }
 
