e00802896f2446012084d8b9ba3ec263ae5eb07e
Ruslan Sakal <ruslan.sakal@gmail.com>
2026-02-08 13:52:42 +0100

    control commit

--- src/_example.ts
+++ src/_example.ts
@@ -4,27 +4,10 @@ class MyFunction extends FunctionRuntime {
   logger = new Logger();
 
   @Handler()
-  main(@Res() res: RunFunctionResponse): void {
-    const bucket: Resource = {
-      resource: {
-        apiVersion: 's3.aws.m.upbound.io/v1beta1',
-        kind: 'Bucket'
-      },
-      connectionDetails: {},
-      ready: Ready.READY_TRUE
-    };
-
-    if (res.desired?.resources) {
-      res.desired.resources['bucker'] = bucket;
-    } else {
-      res.desired = { resources: { bucker: bucket } };
-    }
+  main(@Req() req: RunFunctionRequest, @Res() res: RunFunctionResponse): void {
+    this.logger.info({ req, res }, 'Function Handler (main)');
   }
 
-  @Handler()
-  async mainAsync(@Req() req: RunFunctionRequest, @Res() res: RunFunctionResponse): Promise<void> {
-    this.logger.info({ req, res }, 'Function Handler (mainAsync)');
-  }
 }
 
 new MyFunction();
