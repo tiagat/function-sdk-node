6856c6c9957688c7d4793258b7e04f5798be304d
Ruslan Sakal <ruslan.sakal@gmail.com>
2026-02-17 13:45:55 +0100

    fixed gRPC bind URL

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,19 @@
-const environment = process.env.NODE_ENV || 'production';
+const functionName = process.env.FUNCTION_NAME;
 
 export default {
-  environment,
+  environment: process.env.NODE_ENV || 'production',
   logger: {
     level: process.env.LOG_LEVEL || 'info'
   },
+  function: {
+    name: functionName,
+    revision: process.env.REVISION_NAME,
+    revisionUID: process.env.REVISION_UID
+  },
   grpc: {
-    url: '0.0.0.0:9443',
+    url: functionName ? `dns:///${functionName}.crossplane:9443` : 'localhost:9443',
     tlsCertsDir: process.env.TLS_SERVER_CERTS_DIR,
     checkClientCertificate: true,
-    shutdownTimeout: 10000,
+    shutdownTimeout: 10000
   }
 };
